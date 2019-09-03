e0a1999976aa622065b72b33d0c3fd22c8a6d343
AeroAiro <yulun_wu@berkeley.edu>
2019-09-02 20:41:22 -0700

    added files

--- idnet/alpha_network.py
+++ idnet/alpha_network.py
@@ -16,11 +16,74 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Parameter
-from torch.distributions import RelaxedBernoulli, OneHotCategorical, Normal, MultivariateNormal
+from torch.distributions import RelaxedBernoulli, OneHotCategorical, RelaxedOneHotCategorical, Normal, MultivariateNormal
 from torchvision import datasets, transforms
 import probtorch
 from probtorch.util import expand_inputs
+from probtorch.stochastic import Stochastic, Trace
 from probtorch.objectives.montecarlo import ml
+
+###
+
+class Sample(Stochastic):
+    """Random variables wrap a PyTorch Variable to associate a distribution
+    and a log probability density or mass.
+
+    Parameters:
+        dist(:obj:`Distribution`): The distribution of the variable.
+        value(:obj:`Variable`): The value of the variable.
+        observed(bool): Indicates whether the value was sampled or observed.
+    """
+
+    def __init__(self, dist, value, mask=None, use_pmf=True):
+        self._dist = dist
+        self._value = value
+        self._mask = mask
+        self._use_pmf = use_pmf
+        self._log_prob = None
+
+    @property
+    def dist(self):
+        return self._dist
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def log_prob(self):
+        if self._use_pmf and hasattr(self._dist, 'log_pmf'):
+            self._log_prob = self._dist.log_pmf(self._value)
+        else:
+            self._log_prob = self._dist.log_prob(self._value)
+        return self._log_prob
+
+    @property
+    def mask(self):
+        return self._mask
+
+    def __repr__(self):
+        return "%s Sample containing: %s" % (type(self._dist).__name__,
+                                             repr(self._value))
+
+
+def sample(self, Dist, *args, **kwargs):
+    """Creates a new RandomVariable node"""
+    name = kwargs.pop('name', None)
+    value = kwargs.pop('value', None)
+    dist = Dist(*args, **kwargs)
+    if value is None:
+        value = dist.sample()
+    else:
+        if isinstance(value, Sample):
+            value = value.value
+    node = Sample(dist, value, mask=self._mask)
+    if name is None:
+        self.append(node)
+    else:
+        self[name] = node
+    return value
+Trace.sample = sample
 
 ###
 
@@ -28,7 +91,7 @@ class IDNetwork(nn.Module):
     def __init__(self, num_input,
                  num_hidden,
                  num_output,
-                 temperature=0.33,
+                 temperature=0.1,
                  eps=1e-9):
         super(self.__class__, self).__init__()
         self.probs = nn.Parameter(0.9 * torch.ones(num_input, requires_grad=True))
@@ -60,12 +123,12 @@ class IDNetwork(nn.Module):
         self.eps = eps
 
     @expand_inputs
-    def forward(self, data, labels, num_samples=None):
+    def forward(self, data, labels=None, num_samples=None):
         if num_samples is None:
             indep_shape, dep_dims = [data.shape[0:1], slice(1, data.ndimension())]
         else:
             indep_shape, dep_dims = [data.shape[0:2], slice(2, data.ndimension())]
-        q = probtorch.Trace()
+        q = Trace()
         probs_sig = self.probs
         for d in reversed(range(len(indep_shape))):
             probs_sig = probs_sig.unsqueeze(0).expand(indep_shape[d], *probs_sig.shape)
@@ -79,9 +142,14 @@ class IDNetwork(nn.Module):
             hiddens = m(hiddens)
         outputs = self.output(hiddens)
         probs_lab = outputs.reshape((*indep_shape, *outputs.shape[1:]))
-        q.loss(lambda x_hat, x: -(torch.log(x_hat + self.eps) * x +
-                                  torch.log(1 - x_hat + self.eps) * (1-x)).sum(-1),
-               probs_lab, labels, name='classes')
+        if labels is not None:
+            q.loss(lambda x_hat, x: -(torch.log(x_hat + self.eps) * x +
+                                      torch.log(1 - x_hat + self.eps) * (1-x)).sum(-1),
+                   probs_lab, labels, name='classes')
+        else:
+            q.sample(OneHotCategorical,
+                     probs=probs_lab,
+                     name='classes')
         return q
 
 ###
@@ -102,6 +170,10 @@ def train(data, num_classes, network, optimizer,
         if images.size()[0] == batch_size:
             # count
             N += batch_size
+            # test a number
+            num_classes = 2
+            for i in range(len(labels)):
+                labels[i] = 1 if labels[i] == 3 else 0
             # reshape
             images = images.reshape(images.shape[0], -1)
             labels_onehot = torch.zeros(batch_size, num_classes)
@@ -117,11 +189,12 @@ def train(data, num_classes, network, optimizer,
             # loss
             log_weights = q.log_joint(0, 1, q.conditioned())
             probs_sig = q['signs'].dist.probs
-            loss = - ml(q, 0, 1, log_weights, size_average=True, reduce=True) \
-                   + alpha * F.l1_loss(probs_sig, torch.zeros_like(probs_sig), reduction='mean')
+            loss = (- ml(q, 0, 1, log_weights, size_average=True, reduce=True) +
+                    alpha * F.l1_loss(probs_sig, torch.zeros_like(probs_sig), reduction='mean'))
             # step
             loss.backward()
             optimizer.step()
+            network.probs.data = network.probs.data.clamp(min=0., max=1.)
             # cuda
             if cuda:
                 loss = loss.cpu()
@@ -129,9 +202,9 @@ def train(data, num_classes, network, optimizer,
             epoch_loss += loss.item()
     return epoch_loss / N
 
-def test(data, network,
+def test(data, num_classes, network,
          num_samples=32, batch_size=16, alpha=1.,
-         cuda=False):
+         eps=1e-9, cuda=False):
     network.eval()
     epoch_loss = 0.0
     epoch_correct = 0
@@ -139,13 +212,23 @@ def test(data, network,
     for b, (images, labels) in enumerate(data):
         if images.size()[0] == batch_size:
             N += batch_size
+            # test a number
+            num_classes = 2
+            for i in range(len(labels)):
+                labels[i] = 1 if labels[i] == 3 else 0
+            #
             images = images.reshape(images.shape[0], -1)
+            labels_onehot = torch.zeros(batch_size, num_classes)
+            labels_onehot.scatter_(1, labels.unsqueeze(1), 1)
+            labels_onehot = torch.clamp(labels_onehot, eps, 1-eps)
             if cuda:
                 images = images.cuda()
-            q = network(images, num_samples=num_samples)
+                labels_onehot = labels_onehot.cuda()
+            q = network(images, labels_onehot, num_samples=num_samples)
             log_weights = q.log_joint(0, 1, q.conditioned())
-            batch_loss = - ml(q, 0, 1, log_weights, size_average=True, reduce=True) \
-                         + alpha * F.l1_loss(q['signs'].probs, reduction='mean')
+            probs_sig = q['signs'].dist.probs
+            batch_loss = (- ml(q, 0, 1, log_weights, size_average=True, reduce=True) +
+                          alpha * F.l1_loss(probs_sig, torch.zeros_like(probs_sig), reduction='mean'))
             if cuda:
                 batch_loss = batch_loss.cpu()
             epoch_loss += batch_loss.item()
@@ -170,12 +253,12 @@ def main(args):
     # model parameters
     NUM_PIXELS = 784
     NUM_HIDDEN = 256
-    NUM_DIGITS = 10
+    NUM_DIGITS = 2
 
     # path
     MODEL_NAME = 'mnist-idnetwork-%s' % args.signature
     DATA_PATH = '../dataset'
-    WEIGHTS_PATH = '../model/probtorch'
+    MODEL_PATH = '../model/probtorch'
     PLOTS_PATH = '../plot/probtorch'
 
     # load data
@@ -207,31 +290,40 @@ def main(args):
     if not args.restore:
         for e in range(args.num_epochs):
             train_start = time.time()
-            train_loss, mask = train(train_data, NUM_DIGITS, idnet, optimizer,
-                                     num_samples=args.num_samples, batch_size=args.batch_size, alpha=args.alpha,
-                                     eps=args.epsilon, cuda=CUDA)
-
+            train_loss = train(train_data, NUM_DIGITS, idnet, optimizer,
+                               num_samples=args.num_samples, batch_size=args.batch_size, alpha=args.alpha,
+                               eps=args.epsilon, cuda=CUDA)
             train_end = time.time()
             test_start = time.time()
-            test_loss, test_accuracy = test(test_data, idnet,
+            test_loss, test_accuracy = test(test_data, NUM_DIGITS, idnet,
                                             num_samples=args.num_samples, batch_size=args.batch_size, alpha=args.alpha,
-                                            cuda=CUDA)
+                                            eps=args.epsilon, cuda=CUDA)
             test_end = time.time()
             if not args.mute:
                 print('[Epoch %d] Train: LOSS %.4e (%ds) Test: LOSS %.4e, Accuracy %0.3f (%ds)' % (
                         e, train_loss, train_end - train_start,
                         test_loss, test_accuracy, test_end - test_start))
         if not args.no_sav:
-            if not os.path.isdir(WEIGHTS_PATH):
-                os.mkdir(WEIGHTS_PATH)
+            if not os.path.isdir(MODEL_PATH):
+                os.makedirs(MODEL_PATH)
             torch.save(idnet.state_dict(),
-                       '%s/%s-%s-%s-idnet.rar' % (WEIGHTS_PATH, MODEL_NAME, probtorch.__version__, torch.__version__))
+                       '%s/%s-%s-%s-idnet.rar' % (MODEL_PATH, MODEL_NAME, probtorch.__version__, torch.__version__))
+
+    # save identifiers
+    if CUDA:
+        probs = idnet.probs.cpu().detach().numpy().reshape((28, 28))
+    else:
+        probs = idnet.probs.data.detach().numpy().reshape((28, 28))
+    np.savetxt('%s/%s-idnet-probs.txt' % (MODEL_PATH, MODEL_NAME), probs, delimiter=',')
 
     # analyze result
     if not args.no_vis:
         if not os.path.isdir(PLOTS_PATH):
-            os.mkdir(PLOTS_PATH)
-        '''TODO'''
+            os.makedirs(PLOTS_PATH)
+        fig = plt.figure()
+        plt.imshow(probs, cmap='hot')
+        fig.savefig('%s/%s-heatmap.png' % (PLOTS_PATH, MODEL_NAME), dpi=300)
+
 ###
 
 EXAMPLE_RUN = "example run: python ssvae_rnaseq_probtorch.py --cuda False --seed 0 " \
@@ -245,15 +337,15 @@ if __name__ == "__main__":
                         help="whether to use GPU(s) for training")
     parser.add_argument('--seed', default=0, type=int,
                         help="seed for controlling randomness")
-    parser.add_argument('--temperature', default=0.33, type=float,
+    parser.add_argument('--temperature', default=0.1, type=float,
                         help="temperature of relaxed one-hot distribution")
-    parser.add_argument('--alpha', default=0.0, type=float,
+    parser.add_argument('--alpha', default=.1, type=float,
                         help="relative importance of classification loss")
     parser.add_argument('--beta1', default=0.90, type=float,
                         help="beta1 of Adam optimizer")
     parser.add_argument('--beta2', default=0.999, type=float,
                         help="beta2 of Adam optimizer")
-    parser.add_argument('-e', '--num-epochs', default=5, type=int,
+    parser.add_argument('-e', '--num-epochs', default=1, type=int,
                         help="number of epochs to run")
     parser.add_argument('-s', '--num-samples', default=8, type=int,
                         help="number of samples to draw")
@@ -271,7 +363,7 @@ if __name__ == "__main__":
                         help="do not save model")
     parser.add_argument('--no-vis', action='store_true',
                         help="do not plot result")
-    parser.add_argument('--signature', default="default", type=str,
+    parser.add_argument('--signature', default="alternative", type=str,
                         help="signature for the outputs")
 
     args = parser.parse_args()
